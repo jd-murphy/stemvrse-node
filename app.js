b83c403861a566c6cf425bb9454314448af85444
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-07 15:27:58 -0500

    add auth login

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const admin = require('firebase-admin');
 const serviceAccount = process.env.SERVICEACCOUNT
 const PORT = process.env.PORT || 3000;
 const io = require('socket.io')(server);
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
 
 const config = {
@@ -18,18 +20,63 @@ const config = {
     "messagingSenderId": process.env.MESSAGINGSENDERID,
     "serviceAccount": process.env.SERVICEACCOUNT 
   }
-  
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets/', express.static('./assets'));
 
-app.get('/', (req, res) => {
-    res.send("welcome to stemvrse node app! server is live");
+app.get('/', (req, res) => {     
+    // Initialize the FirebaseUI Widget using Firebase.
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            if (authResult.isAdmin) {
+                dest = '/dashboard'
+            } else {
+                dest = '/home'
+            }
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: dest,
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+    res.sendFile('login.html',{root: __dirname});
+    // res.send("welcome to stemvrse node app! server is live");
+
+});
+
+app.get('/home', (req, res) => {
+    console.log("\n\nGET /home   - user login \n\n")
+    res.sendFile('index.html',{root: __dirname});
 });
 
 app.get('/dashboard', (req, res) => {
-    console.log("\n\nGET /dashboard \n\n")
+    console.log("\n\nGET /dashboard   - admin login \n\n")
     res.sendFile('index.html',{root: __dirname});
 });
 
