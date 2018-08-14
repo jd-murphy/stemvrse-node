831b6a5b2a8204eb2f9acbead8755caf3a4995c6
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-13 20:49:59 -0500

    passing arguments to onclick handler in <img> for fav

--- scripts/home.js
+++ scripts/home.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
                                         '</div>'
 
                     $('#displayVideos').append('<li><div class="card border-info mb-3" id="' + strippedName + '" data-descriptor="video-li" data-video-name="' + name + '" style="max-width:' + cardMaxWidth + ';">' + 
-                            '<div class="card-header"><strong>' + name + '<a class="favoriteVideo" data-video-display-name="' + name + '" href="#" id="favoriteVideo' + strippedName + '" style="float: right;">' + '<img id="favButton' + strippedName + '" src="/assets/favorite.png" width="16px" height="16px" onclick="favMe(' + strippedName +", " + name + ');"></a>' + '</strong></div>' + 
+                            '<div class="card-header"><strong>' + name + '<a class="favoriteVideo" data-video-display-name="' + name + '" href="#" id="favoriteVideo' + strippedName + '" style="float: right;">' + '<img id="favButton' + strippedName + '" src="/assets/favorite.png" width="16px" height="16px" onclick="favMe(' + strippedName, name + ');"></a>' + '</strong></div>' + 
                             '<div class="card-body">' + 
                             ' <h4 class="card-title">Video Info</h4>' + 
                             ' <div>' + 
