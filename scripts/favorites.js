7b38677616d8c9560f55f7babc805c94db9778c8
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-14 21:45:46 -0500

    hide videos on favorites page

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
                     lname = name.toLowerCase();
                     strippedName =  lname.replace(/[^a-z0-9]/g, '');
 
-                    $('#displayVideos').append('<li><div class="card border-info mb-3 hideMe" id="' + strippedName + '" data-descriptor="video-li" data-video-name="' + name + '" style="max-width:' + cardMaxWidth + ';">' + 
+                    $('#displayVideos').append('<li class="hideMe"><div class="card border-info mb-3" id="' + strippedName + '" data-descriptor="video-li" data-video-name="' + name + '" style="max-width:' + cardMaxWidth + ';">' + 
                             '<div class="card-header"><strong>' + name + '<a class="favoriteVideo" href="#favoriteVideo' + strippedName + '" id="favoriteVideo' + strippedName + '" style="float: right;">' + '<img id="favButton' + strippedName + '" src="/assets/favorite.png" data-video-display-name="' + name + '" width="16px" height="16px"></a>' + '</strong></div>' + 
                             '<div class="card-body">' + 
                             ' <h4 class="card-title">Video Info</h4>' + 
@@ -58,7 +58,7 @@ $(document).ready(function(){
                 console.log(faves);
                 for (var i = 0; i < faves.length; i++) {
                     $('#favButton' + faves[i]).attr('src', "/assets/favorited.png") // favorited video
-                    $('#' + faves[i]).classList.remove("hideMe");
+                    $('#' + faves[i]).parent().removeClass("hideMe");
                 }
                 localStorage.setItem(user, JSON.stringify(faves));
             } else {
