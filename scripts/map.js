17e9736ab812f788df1eb69e269fcc9bfa685095
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-16 14:36:59 -0500

    testing add nyc to map

--- scripts/map.js
+++ scripts/map.js
@@ -13,20 +13,26 @@ $(document).ready(function(){
         const myProjection = d3.geoNaturalEarth1()
         const path = d3.geoPath().projection(myProjection)
         const graticule = d3.geoGraticule()
-        var coordinates = myProjection([-112.0785,33.46764]);
+        var nyc = {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [-74.0060, 40.7128]
+            },
+            "properties": {
+              "name": "New York City"
+            }
+          }
+       
    
 
         function drawMap(err, world) {
             if (err) throw err
-            // svg.append("path")
-            //   .datum(graticule)
-            //   .attr("class", "graticule")
-            //   .attr("d", path);
-            svg.append("circle")
-                .attr("cx", coordinates[0])
-                .attr("cy", coordinates[1])
-                .attr("r", 5)
-                .style("fill", "red");
+            
+            svg.append("path")
+              .datum(nyc)
+              .attr("d", path);
+           
             svg.append("path")
                 .datum(graticule.outline)
                 .attr("class", "foreground")
