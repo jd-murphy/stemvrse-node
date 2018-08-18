69b9185f9cd82ac2b1f448c091610371b5572680
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-18 14:42:55 -0500

    app.set and app.use "styles"

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, "views"));
 app.set('scripts', path.join(__dirname, "scripts"));
+app.set('styles', path.join(__dirname, "styles"));
   
 
 
@@ -61,6 +62,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets/', express.static('./assets'));
 app.use('/scripts/', express.static('./scripts'));
+app.use('/styles/', express.static('./styles'));
 
 
 
