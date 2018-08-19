3a7bb9ae2ec792e19bd5103c8c2d0174d5f81668
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-19 00:01:12 -0500

    format map view on video page

--- scripts/video.js
+++ scripts/video.js
@@ -102,7 +102,7 @@ $(document).ready(function(){
                 
                
             }
-            var mymap = L.map('mapid').setView([lat, lng], 10);
+            var mymap = L.map('mapid').setView([lat, lng], 6);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
