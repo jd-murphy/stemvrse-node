9c6679bcea48e55a82a5ef6ba475c678539d8614
Jordan Murphy <jordan@blueberri-saas.com>
2018-08-18 23:46:05 -0500

    format map view

--- scripts/video.js
+++ scripts/video.js
@@ -102,10 +102,9 @@ $(document).ready(function(){
                 
                
             }
-            var mymap = L.map('mapid').setView([lat, lng], 13);
+            var mymap = L.map('mapid').setView([lat, lng], 25);
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
                 id: 'mapbox.streets',
                 accessToken: 'pk.eyJ1IjoiamRtdXJwaHk1MjEiLCJhIjoiY2psMDg3NWYxMTIzbDNrcWtrdmFybHR6bSJ9.r4UtNU5328E6u70IAsarTQ'
             }).addTo(mymap);
